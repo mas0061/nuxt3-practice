bba1e130d83b8b737122a01ca557daaa456148cc
mas0061 <mas0061@users.noreply.github.com>
2023-02-25 17:12:43 +0900

    feat: pagesとlayoutsまで

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button @click="enableCustomLayout">Update layout</button>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: false
+})
+
+const route = useRoute()
+
+const enableCustomLayout = () => {
+  route.meta.layout = 'custom'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NuxtLayout name="default">
+      <template #header>ヘッダー</template>
+      <h1>Main page</h1>
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: false
+})
+</script>
