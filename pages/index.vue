259551a37194278345f0c5d9f7ca4937ac3450a5
mas0061 <mas0061@users.noreply.github.com>
2023-02-26 10:43:59 +0900

    feat: pluginsディレクトリまで

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,29 @@
 <template>
   <div>
       <h1>Main page</h1>
+      <h2>{{ $hello('Wolrd') }}</h2>
       <div>
         <img src="/icon.png" alt="Nuxt3 icon" />
       </div>
-      <button @click="handleClick">Coupon get</button>
-      <LazyCoupon v-if="show" />
+      <div>
+        <button @click="handleClick">Coupon get</button>
+        <LazyCoupon v-if="show" />
+      </div>
+      <div>
+        <div>Count: {{  count }}</div>
+        <button @click="() => inc()">increase</button>
+        <button @click="() => dec()">decrease</button>
+      </div>
   </div>
 </template>
 
 <script setup>
+const { $hello } = useNuxtApp()
 const show = ref(false)
 
 const handleClick = () => {
   show.value = true
 }
+
+const { count, inc, dec } = useCounter(100)
 </script>
